7032beb1278c4de1958e76ee72acd0276e06c9a9
ak89mkii <ak89mkii@gmail.com>
2024-01-11 15:47:15 -0500

    Dev environment and db configured.

--- dicedex/settings.py
+++ dicedex/settings.py
@@ -90,6 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
     }
 }
 
