14a9957bc59edb76600248a47b82bfc31c7164c4
ak89mkii <ak89mkii@gmail.com>
2024-01-30 10:25:07 -0500

    Successfully deployed to Heroku.

--- dicedex/settings.py
+++ dicedex/settings.py
@@ -90,12 +90,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DATABASE_NAME'],
+        # Comment out for local development.
         # 'USER': os.environ['USER'],
         # 'PASSWORD': os.environ['PASSWORD'],
     }
 }
 
-# # Comment out for local development.
+# Comment out for local development.
 import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
