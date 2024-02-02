0b037d5ff79fc7d6924fd0c712312906a9d4a991
ak89mkii <ak89mkii@gmail.com>
2024-02-02 11:17:41 -0500

    Added new 'Link' model.

--- dicedex_app/admin.py
+++ dicedex_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Item, Theme
+from .models import Link, Item, Theme
 
 # Register your models here.
+admin.site.register(Link)
 admin.site.register(Item)
 admin.site.register(Theme)
