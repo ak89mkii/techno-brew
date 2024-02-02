13bef9466b34de6b43177a6dec7fd19b289f1375
ak89mkii <ak89mkii@gmail.com>
2024-02-02 13:41:42 -0500

    Added new 'Link' model.

--- dicedex_app/models.py
+++ dicedex_app/models.py
@@ -52,7 +52,22 @@ CARDS = (
 
 
 class Link(models.Model):
-    label = models.CharField(max_length=100, default="None")
+    description = models.CharField(verbose_name='Description (Link Share) ', max_length=1000, default="None")
+    link = models.CharField(verbose_name='Link (Link Share)', max_length=1000, default="None")
+    color = models.CharField(
+        max_length=30,
+        choices=CARDS,
+        default=CARDS[0][0]
+    )
+    timestamp = models.DateField(auto_now_add=True)
+    # Also for "owner field"
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.label
+
+    def get_absolute_url(self):
+        return reverse('home_logged_in')
 
 
 class Item(models.Model):
