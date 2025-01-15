7a8c0e9c4709afa73a53338cbe1d9f1b3f3ae46a
ak89mkii <ak89mkii@gmail.com>
2025-01-15 15:32:36 -0500

    Completed 'Membership Information' card.

--- dicedex_app/views.py
+++ dicedex_app/views.py
@@ -111,7 +111,7 @@ def library_demo_search(request):
         elif search_type == 'color':
             results = Item.objects.filter(color__icontains=query)
 
-    return render(request, 'library/demo.html', {'form': form, 'results': results})
+    return render(request, 'library/demo_search.html', {'form': form, 'results': results})
 
 # # SECTION LISTS: Personal Items
 # @login_required
