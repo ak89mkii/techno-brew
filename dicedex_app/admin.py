8e699c52a9a6eb01ae33025ac460bdafa0c96030
ak89mkii <ak89mkii@gmail.com>
2025-01-23 17:24:10 -0500

    AAdded new models to admin.py.

--- dicedex_app/admin.py
+++ dicedex_app/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
-from .models import Link, Item, Theme
+from .models import Link, Item, Theme, Member, Dog, Facility
 
 # Register your models here.
 admin.site.register(Link)
 admin.site.register(Item)
 admin.site.register(Theme)
+admin.site.register(Member)
+admin.site.register(Dog)
+admin.site.register(Facility)
+
 
 # admin.site.register(Gundam)
 
