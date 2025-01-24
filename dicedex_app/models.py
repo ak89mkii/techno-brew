12d3e1fbb77417141c1d37a6890f57b8107d9892
ak89mkii <ak89mkii@gmail.com>
2025-01-24 13:00:54 -0500

    Added new models to admin.py.

--- dicedex_app/models.py
+++ dicedex_app/models.py
@@ -188,7 +188,7 @@ class Member(models.Model):
 
     # This changes the displayed text of the objects in Django admin to the declared field (label).
     def __str__(self):
-        return f"{self.last_name}, {self.first_name}
+        return f"{self.last_name}, {self.first_name}"
 
     def get_absolute_url(self):
         return reverse('home_logged_in')
