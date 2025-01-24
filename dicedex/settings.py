aff7fa4e574f2bd78584d3193c60aa96359ff675
ak89mkii <ak89mkii@gmail.com>
2025-01-24 15:09:50 -0500

    Added new models to admin.py.

--- dicedex/settings.py
+++ dicedex/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
